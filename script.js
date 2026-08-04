766911f1af95b6ffbc1213e6ab84604fbeba17f6
Daniel Alejandro Ovalle Tavera <daniel.ovalle.dev@outlook.es>
2026-08-04 11:39:34 -0500

    Se agrego contenido en el prototipo

---
+++ script.js
@@ -0,0 +1,15 @@
+function cambiarMensaje() {
+    const mensaje = document.getElementById("mensaje");
+
+    mensaje.textContent = "¡Bienvenido a AulaConecta! Tu información está lista para consultar.";
+}
+function cambiarBoton() {
+    const boton = document.getElementById("boton");
+
+    boton.textContent = "¡Listo!";
+    boton.style.backgroundColor = "#198754";
+}
+document.getElementById("boton").addEventListener("click", function() {
+    cambiarMensaje();
+    cambiarBoton();
+});
